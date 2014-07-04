d1e1f991466f26fe83f622221d7006ca48bcd08f
Yankovsky <YankovskyAndrey@gmail.com>
2014-07-04 18:05:35 +0400

    update dropdown.js to new version

--- dropdown.js
+++ dropdown.js
@@ -1,48 +1,104 @@
-angular.module('ya.dropdown', []).directive('yaDropdown', function($document, $timeout) {
+angular.module('ya.dropdown', []).directive('yaDropdown',function($document, $animate, $timeout, $parse) {
   var ESCAPE_KEY_CODE = 27
   return {
-    restrict: 'A',
-    scope: {
-      yaDropdown: '='
-    },
-    link: function(scope, element, attrs) {
-      scope.$watch(function() {
-        return !!scope.yaDropdown
-      }, function() {
-        if (scope.yaDropdown) {
+    restrict: 'C',
+    require: '^yaDropdown',
+    controller: function($scope) {
+      var that = this
+      that.dropdownElement = null
+      that.dropdownContentElement = null
+      that.isOpen = false
+
+      var getIsOpen, setIsOpen = angular.noop
+
+      that.init = function(element, isOpenAttr) {
+        that.dropdownElement = element
+        if (isOpenAttr) {
+          getIsOpen = $parse(isOpenAttr)
+          setIsOpen = getIsOpen.assign
+
+          $scope.$watch(function() {
+            return !!getIsOpen($scope)
+          }, function(value) {
+            that.isOpen = value
+          })
+        }
+      }
+
+      that.toggle = function() {
+        that.isOpen = !that.isOpen
+      }
+
+      $scope.$watch(function() {
+        return that.isOpen
+      }, function(isOpen) {
+        $animate[isOpen ? 'addClass' : 'removeClass'](that.dropdownElement, 'ya-dropdown-open')
+
+        if (isOpen) {
           subscribe()
         } else {
           unsubscribe()
         }
-      }, true)
-
-      function hideDropdown() {
-        scope.yaDropdown = false
-        scope.$apply()
-      }
+        setIsOpen($scope, isOpen);
+      })
 
       function subscribe() {
         $timeout(function() {
           $document.on('click', onClickOutside).on('keydown', onKeydown)
-          scope.$on('$destroy', unsubscribe)
+          that.dropdownContentElement.on('click', stopPropagation)
+          $scope.$on('$destroy', unsubscribe)
         })
       }
 
       function unsubscribe() {
         $document.off('click', onClickOutside).off('keydown', onKeydown)
+        that.dropdownContentElement.off('click', stopPropagation)
+      }
+
+      function stopPropagation() {
+        return false
       }
 
       function onClickOutside() {
-        hideDropdown()
-        unsubscribe()
+        that.isOpen = false
+        $scope.$apply()
       }
 
       function onKeydown(e) {
         if (e.keyCode === ESCAPE_KEY_CODE) {
-          hideDropdown()
-          unsubscribe()
+          that.isOpen = false
+          $scope.$apply()
         }
       }
+    },
+    link: function(scope, element, attrs, yaDropdownCtrl) {
+      yaDropdownCtrl.init(element, attrs.isOpen)
     }
   }
-})
+}).directive('yaDropdownToggle',function() {
+    return {
+      restrict: 'C',
+      require: '^yaDropdown',
+      link: function(scope, element, attrs, yaDropdownCtrl) {
+        function toggle(event) {
+          event.preventDefault()
+          yaDropdownCtrl.toggle()
+          scope.$apply()
+        }
+
+        element.on('click', toggle)
+
+        scope.$on('$destroy', function() {
+          element.off('click', toggle)
+        })
+      }
+    }
+  }).directive('yaDropdownContent', function() {
+    return {
+      restrict: 'C',
+      require: '^yaDropdown',
+      link: function(scope, element, attrs, yaDropdownCtrl) {
+        yaDropdownCtrl.dropdownContentElement = element
+      }
+    }
+  })
